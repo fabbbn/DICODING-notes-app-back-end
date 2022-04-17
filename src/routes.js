91890dc2d0fd8e83950af4ed7bb1892787dec4df
fabbbn <dev.nabilaf@gmail.com>
2022-04-17 11:03:01 +0700

    fix permission bug and clean code using eslint

--- src/routes.js
+++ src/routes.js
@@ -1,38 +1,38 @@
-const { 
-    addNoteHandler,
-    getAllNotesHandler,
-    getNoteByIdHandler,
-    editNoteByIdHandler,
-    deleteNoteByIdHandler,
-} = require("./handler");
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
 
 const routes = [
-    // membuat catatan baru
-    {
-        method: 'POST',
-        path: '/notes',
-        handler: addNoteHandler
-    },
-    {
-        method: 'GET',
-        path: '/notes',
-        handler: getAllNotesHandler
-    },
-    {
-        method: 'GET',
-        path: '/notes/{id}',
-        handler: getNoteByIdHandler
-    },
-    {
-        method: 'PUT',
-        path: '/notes/{id}',
-        handler: editNoteByIdHandler
-    },
-    {
-        method: 'DELETE',
-        path: '/notes/{id}',
-        handler: deleteNoteByIdHandler
-    },
+  // membuat catatan baru
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
 ];
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
 const Hapi = require('@hapi/hapi');
 
 const routes = require('./routes');
-const notes = require("./notes");
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: "localhost",
-        routes: {
-            cors: {
-                origin: ['*'],
-            },
-        },
-    });
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-    server.route(routes);
+  server.route(routes);
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  process.stdout.write(`Server berjalan pada ${server.info.uri}`);
 };
 
 // emmiter unhandled exception, close the program
